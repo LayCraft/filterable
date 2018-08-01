be46c9a0b541857395c3d0ffb0f31060838b0649
Curtis LayCraft <curtis.laycraft@zoho.com>
2018-07-31 23:25:08 -0700

    filtering on both label and value

--- src/app/filter-box/filter-box.component.ts
+++ src/app/filter-box/filter-box.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+const LIST=[
+  {
+    status:"active", 
+    sublist:[
+      {label:"bork", value:"qwe"},
+      {label:"tork", value:"123"},
+    ]},
+  {
+    status:"active", 
+    sublist:[
+      {label:"bork", value:"asd"},
+      {label:"scant", value:"123"},
+    ]},
+];
 
 @Component({
   selector: 'app-filter-box',
@@ -6,10 +21,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-box.component.css']
 })
 export class FilterBoxComponent implements OnInit {
-  input: string = "YOLO";
-  constructor() { }
+  label: string;
+  value: string;
+  list;
+  filterList;
+  finalFilterList;
+  constructor() { 
+    this.list = LIST;
+  }
 
   ngOnInit() {
   }
-
+  filterLabel(l: string){
+    let filterList = [];
+    this.label = l;
+    this.list.forEach(obj => {
+      // alert(JSON.stringify(obj));
+      obj.sublist.forEach(element => {
+        if(element.label===l) filterList.push(obj);
+      });
+    });
+    this.filterList = filterList;
+  }
+  filterValue(v: string){
+    this.value = v;
+    let filterList = [];
+    this.list.forEach(obj => {
+      // alert(JSON.stringify(obj));
+      obj.sublist.forEach(element => {
+        if(element.value===v) filterList.push(obj);
+      });
+    });
+    this.finalFilterList = filterList;
+  }
 }
